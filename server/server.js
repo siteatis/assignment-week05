d716f5de11773bcc1c4e1aae534b4a116ec94f7a
lukeplechaty <lukeplechaty@hotmail.co.uk>
2025-05-28 11:48:50 +0100

    add: server setup for messages and added messages/messageboards defer

---
+++ server/server.js
@@ -0,0 +1,65 @@
+import express from "express";
+import dotenv from "dotenv";
+import pg from "pg";
+import cors from "cors";
+
+dotenv.config();
+const db = new pg.Pool({ connectionString: process.env.database_url });
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.listen(3000, () => console.log("Server running on 3000"));
+
+app.post("/addMsg", async (req, res) => {
+  const body = req.body;
+  const query = await db.query(
+    `INSERT INTO messages (username, message, timestamp, messageboardid) VALUES ($1, $2, $3, $4) RETURNING id`,
+    [body.user_name, body.message, body.time_stamp, body.message_board_id]
+  );
+  res.json(query.rows);
+});
+
+app.post("/addMsgBoard", async (req, res) => {
+  const body = req.body;
+  const query = await db.query(
+    `INSERT INTO messageboards (name, updated) VALUES ($1, $2) RETURNING id`,
+    [body.user_name, body.time_stamp]
+  );
+  res.json(query.rows);
+});
+
+app.get("/getMsgs", async (req, res) => {
+  const query = await db.query(`SELECT * FROM messages ORDER BY id ASC`);
+  let data = query.rows;
+  res.json(data);
+});
+
+app.get("/getMsgBoards", async (req, res) => {
+  const query = await db.query(`SELECT * FROM messageboards ORDER BY id ASC`);
+  let data = query.rows;
+  res.json(data);
+});
+
+app.put("/updateMsgBoard/:id/:timestamp", (req, res) => {
+  const params = req.params;
+  const query = db.query(
+    `UPDATE messageboards SET updated=($1) WHERE id=($2)`,
+    [params.timestamp, params.id]
+  );
+  res.json(query);
+});
+
+app.delete("/deleteMsg/:id", (req, res) => {
+  const params = req.params;
+  const query = db.query(`DELETE FROM messages WHERE id=($1)`, [params.id]);
+  res.json(query);
+});
+
+app.delete("/deleteMsgBoard/:id", (req, res) => {
+  const params = req.params;
+  const query = db.query(`DELETE FROM messageboards WHERE id=($1)`, [
+    params.id,
+  ]);
+  res.json(query);
+});
